dfae7568927d1ce0c031437c598aa36442612be9
JCPRGMR <juancardedon@gmail.com>
2024-03-01 16:09:32 -0400

    PROBLEMA 1: El input decomercial no permite entrada de valoes (SOLUCIONAR), PROBLEMA 2. Eliminar pase

--- js/Titulo_programa.js
+++ js/Titulo_programa.js
@@ -1,20 +1,24 @@
-
-setInterval(Titulo, 10)
-
-function Titulo(){
-    var programa = document.getElementById("programa")
-    var response = new XMLHttpRequest()
-    // response.open("GET","../Json/Programa_input.php",true)
-    response.open("GET","../Json/Programa_input.php",true)
-    response.onload = function(){
-        try {
-            if(response.status === 200){
-                console.log(response.response)
-                programa.value = response.response;
+var programa = document.getElementById("programa")
+if(programa.value.length > 0){
+    
+}else{
+    setInterval(Titulo, 10)
+    
+    function Titulo(){
+        var programa = document.getElementById("programa")
+        var response = new XMLHttpRequest()
+        // response.open("GET","../Json/Programa_input.php",true)
+        response.open("GET","../Json/Programa_input.php",true)
+        response.onload = function(){
+            try {
+                if(response.status === 200){
+                    console.log(response.response)
+                    programa.value = response.response;
+                }
+            } catch (error) {
+                console.log(error)
             }
-        } catch (error) {
-            console.log(error)
         }
+        response.send()
     }
-    response.send()
 }
